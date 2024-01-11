f8184a0b18acada9e5e743d7f207326db6c8b7f7
Escapay-repo <escapayrepo@gmail.com>
2024-01-11 14:03:16 -0300

    a

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ class App {
 
     middlewares() {
         this.server.use('*', cors({
-            // origin: 'https://gusmfscoder.com.br',
-            origin: 'http://localhost:4200',
+            origin: 'escapaybank-integracoes.com.br',
+            //origin: 'http://localhost:4200',
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true,
